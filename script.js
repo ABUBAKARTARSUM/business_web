f066268416a6e60a2a1066c3bf5651485dee30a8
ABUBAKARTARSUM <80694589+ABUBAKARTARSUM@users.noreply.github.com>
2023-09-06 08:57:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const numberPax = document.getElementById('numberPax').ariaValueText;
+const resultPrice = document.getElementById('resultPrice');
+const btnPrice = document.getElementById('btnPrice');
+btnPrice.addEventListener('click', () => {});
